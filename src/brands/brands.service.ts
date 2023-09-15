d809f02394353f2b616b60624448c90c49f0f52d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-15 11:17:49 +0200

    feat: findOne service

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+
   create(createBrandDto: CreateBrandDto) {
     return 'This action adds a new brand';
   }
@@ -12,7 +22,10 @@ export class BrandsService {
     return `This action returns all brands`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) throw new NotFoundException(`Brand with id ${id} not found`);
+
     return `This action returns a #${id} brand`;
   }
 

9c2305f6e02469f75b6dd60443919c4e7cd10885
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-15 09:06:14 +0200

    feat: add IsString to CreateCarDto

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCarDto {
   @IsNotEmpty()
+  @IsString()
   readonly brand: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly model: string;
 }

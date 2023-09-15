c458ddf5a37749ed064f676d3936c736875069b5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-15 10:02:30 +0200

    feat: UpdateCarDto

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -13,12 +13,16 @@ export class CreateCarDto {
 export class UpdateCarDto {
   @IsString()
   @IsUUID()
+  @IsOptional()
+  @IsNotEmpty()
   readonly id: string;
 
+  @IsString()
   @IsNotEmpty()
   @IsOptional()
   readonly brand: string;
 
+  @IsString()
   @IsNotEmpty()
   @IsOptional()
   readonly model: string;

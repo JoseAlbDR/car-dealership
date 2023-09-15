bbf70949df2aca31d42a02c412d835a6d3f97c7a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-15 10:02:14 +0200

    feat: checkCar to check if a car already exists

---
+++ src/cars/utils/cars.checkCar.ts
@@ -0,0 +1,15 @@
+import { ICar, ICheckCar } from '../interfaces/car.interface';
+import { ConflictException } from '@nestjs/common';
+
+export const checkCar = (cars: ICar[], checkCar: ICheckCar) => {
+  const alreadyExist = cars.some(
+    (car) => car.brand === checkCar.brand && checkCar.model === checkCar.model,
+  );
+
+  console.log(alreadyExist);
+
+  if (alreadyExist)
+    throw new ConflictException(
+      `Car with brand: ${checkCar.brand} and model: ${checkCar.model} already exists`,
+    );
+};
